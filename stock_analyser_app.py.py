17d19789245015fdfdce117753a420d71db16f97
Jaimin Patel <37634919+Jaimin09@users.noreply.github.com>
2020-09-06 19:44:11 +0530

    Update stock_analyser_app.py.py

--- stock_analyser_app.py.py
+++ stock_analyser_app.py.py
@@ -10,16 +10,25 @@ def inputurl():
         try :
             url=request.form["url"]
 
-            score = str(get_score(url))
+            score = get_score(url)
 
-            rating, info, lst_yr_info = get_company_details_and_rating(url)
+            cmp_name, rating, info, lst_yr_info = get_company_details_and_rating(url)
 
             ls_price = lst_yr_info[0]
             curr_price = lst_yr_info[1]
             gain = lst_yr_info[2]
+
+            if(score > 300):
+                clr = 'green'
+            elif(score < 300 and score > 0):
+                clr = '#ffc40c'
+            else:
+                clr = 'red'
+
+            score = str(score)
             
-            return render_template("analyse_stock.html",score = score, rating = rating, info = info,
-                               ls_price = ls_price, curr_price = curr_price, gain = gain)
+            return render_template("analyse_stock.html",cmp_name = cmp_name, score = score, rating = rating, info = info,
+                               ls_price = ls_price, curr_price = curr_price, gain = gain, clr = clr)
 
         except UnboundLocalError:
             return render_template("error.html", msg = "Our algorithm couldn't get the analyse this stock. Make sure the company-stock is not of any kind of bank's, as banks are currently not supported.")
